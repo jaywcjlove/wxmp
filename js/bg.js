6b88d029da66aa3a59b4551b5910923cc3bd819a
jaywcjlove <398188662@qq.com>
2016-02-25 18:09:47 +0800

    更改代码高亮样式！

--- js/bg.js
+++ js/bg.js
@@ -9,7 +9,7 @@ chrome.extension.onRequest.addListener(
                     for (var i = 0; i < elms.length; i++) {
                         var styl = elms[i].getAttribute('style');
                         if(elms[i].tagName === 'PRE'){
-                            elms[i].setAttribute('style',' word-wrap: normal!important;box-sizing: border-box; overflow: auto;font-size: 0.93em; padding: 1em; margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.3; word-break: break-all; word-wrap: break-word; color: rgb(101, 123, 131); border: none; border-radius: 3px; max-height: 35em;  background-color: rgb(246, 246, 246);') 
+                            elms[i].setAttribute('style',' word-wrap: normal!important;box-sizing: border-box; overflow: auto;font-size: 0.93em; padding: 1em; margin-top: 1.5em; margin-bottom: 1.5em; line-height: 1.3; word-break: break-all; word-wrap: break-word; color: rgb(101, 123, 131); border: none; border-radius: 3px; max-height: 35em;  background-color:#E1E1E9;') 
                         }else if(elms[i].tagName === 'CODE'){
                             elms[i].setAttribute('style','box-sizing: border-box;font-size: 1em; color: inherit; border-radius: 0px; white-space: inherit; overflow-wrap: normal; background: none;') 
                         }else{
